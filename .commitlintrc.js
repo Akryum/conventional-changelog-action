fccc528bc8660fba1d395f81e52d46418543cf5a
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-12-28 20:36:26 +0100

    chore: use yarn

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,12 +2,4 @@ module.exports = {
   extends: [
     '@commitlint/config-conventional',
   ],
-
-  rules: {
-    'subject-case': [
-      2,
-      'always',
-      'sentence-case',
-    ],
-  },
 }
